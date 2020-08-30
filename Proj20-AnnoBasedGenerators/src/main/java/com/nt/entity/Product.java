08e44da67501556dc8bc768bdf8136f97a1aa419
Mukesh Patel <patelmukesh325@gmail.com>
2020-08-30 14:20:00 +0530

    generators

--- Proj20-AnnoBasedGenerators/src/main/java/com/nt/entity/Product.java
+++ Proj20-AnnoBasedGenerators/src/main/java/com/nt/entity/Product.java
@@ -7,6 +7,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
 
 import lombok.Data;
 
@@ -14,8 +15,29 @@ import lombok.Data;
 @Entity
 public class Product implements Serializable {
 	
-	@Id
+	/*@Id
 	@GenericGenerator(name="gen1",strategy = "increment")
+	@GeneratedValue(generator = "gen1")
+	*/
+	/*
+	@Id
+	@GenericGenerator(name="gen1",strategy = "identity")
+	@GeneratedValue(generator = "gen1")
+	*/
+	/*
+	@Id
+	@GenericGenerator(name="gen1",strategy = "sequence")
+	@GeneratedValue(generator = "gen1")
+	*/
+	/*
+	@Id
+	@GenericGenerator(name="gen1",strategy = "sequence",parameters = @Parameter(name="sequence_name",value="pid_seq"))
+	*/
+	
+	@Id
+	@GenericGenerator(name="gen1",strategy = "seqhelo",parameters = {@Parameter(name="sequence_name",value="pid_seq"),
+	                                                                                                                                        @Parameter(name="max_lo",value="5")})
+	                                                                                                                                        
 	@GeneratedValue(generator = "gen1")
 	private int pid;
 	private String pname;
